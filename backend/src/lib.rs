c25ca49116fb155e512bc86583ce023076e7e750
Carson Yan <carson.yan@crypto.com>
2019-08-06 12:39:23 +0800

    clean up

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,31 +1,5 @@
 #[macro_use]
 extern crate diesel;
 
-extern crate dotenv;
-
 pub mod models;
 pub mod schema;
-
-use diesel::prelude::*;
-use dotenv::dotenv;
-use std::env;
-
-
-pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-}
-
-// pub fn create_post(conn: &SqliteConnection, title: &str, body: &str) -> usize {
-//     use schema::posts;
-
-//     let new_post = NewPost { title, body };
-
-//     diesel::insert_into(posts::table)
-//         .values(&new_post)
-//         .execute(conn)
-//         .expect("Error saving new post")
-// }
